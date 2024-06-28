df39cebd12c5d2c97699045242e1a3335cb778c8
Thiago Seito Satake <thiago.seito.satake@gmail.com>
2024-06-27 21:50:22 -0300

    changes themes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/auth-guard.service';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard] },
     { path: 'premium', loadChildren: () => import('./modules/premium/premium.module').then(m => m.PremiumModule), canActivate: [AuthGuard] },
     { path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule), canActivate: [AuthGuard] }
